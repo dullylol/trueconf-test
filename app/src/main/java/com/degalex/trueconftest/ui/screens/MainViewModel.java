0693f5efe7da61b9369e5b543daad2ffe654bc81
Alex <oleksii.dehtiarov@nixsolutions.com>
2022-05-13 00:11:45 +0300

    TC-3 Hello screen

--- app/src/main/java/com/degalex/trueconftest/ui/screens/MainViewModel.java
+++ app/src/main/java/com/degalex/trueconftest/ui/screens/MainViewModel.java
@@ -1,6 +1,9 @@
 package com.degalex.trueconftest.ui.screens;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.degalex.trueconftest.ui.base.BaseViewModel;
+import com.degalex.trueconftest.ui.navigation.ActivityHolder;
 
 import javax.inject.Inject;
 
@@ -10,9 +13,26 @@ import dagger.hilt.android.lifecycle.HiltViewModel;
 class MainViewModel extends BaseViewModel {
 
     private MainRouter router;
+    private ActivityHolder activityHolder;
 
     @Inject
-    public MainViewModel(MainRouter router) {
+    public MainViewModel(
+            MainRouter router,
+            ActivityHolder activityHolder
+    ) {
         this.router = router;
+        this.activityHolder = activityHolder;
+    }
+
+    public void onAttachActivity(AppCompatActivity activity) {
+        activityHolder.attachActivity(activity);
+    }
+
+    public void onDetachActivity() {
+        activityHolder.detachActivity();
+    }
+
+    public void onFirstLaunch() {
+        router.openHelloScreen();
     }
 }
